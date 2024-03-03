4244eb5e4ff0fc9f96e7e291502b48133c08cb84
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2024-03-02 21:55:58 -0800

    add logo

--- src/shared/components/Logo/Styles.ts
+++ src/shared/components/Logo/Styles.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const StyledLogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
   cursor: pointer;
   box-sizing: border-box;
 `;
--- src/shared/components/Logo/index.tsx
+++ src/shared/components/Logo/index.tsx
@@ -3,6 +3,8 @@ import { StyledLogoContainer } from "./Styles";
 import { headerBoldMedium } from "../../utils/fonts";
 import styled from "styled-components";
 
+import LogoSvg from "./LogoSvg";
+
 interface Props {
   variant: string;
 }
@@ -11,10 +13,18 @@ const Logo = ({ variant }: Props) => {
   const navigate = useNavigate();
   return (
     <StyledLogoContainer onClick={() => navigate("/")}>
+      <SVGContainer>
+        <LogoSvg />
+      </SVGContainer>
       <StyledLogoText>Designify</StyledLogoText>
     </StyledLogoContainer>
   );
 };
+
+const SVGContainer = styled.div`
+  width: 40px;
+  height: 40px;
+`;
 
 const StyledLogoText = styled.h1`
   ${headerBoldMedium}
